50bbfedbe6517056a3de69b289475d3f7ad1c47e
khw3754 <84065420+khw3754@users.noreply.github.com>
2023-04-21 15:50:34 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 9.98 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
@@ -0,0 +1,12 @@
+def solution(n,a,b):
+    answer = 1
+
+    while True:
+        if abs(a - b) == 1 and min(a, b) % 2 == 1:
+            break
+        
+        a = a//2 if a%2 == 0 else a//2 + 1
+        b = b//2 if b%2 == 0 else b//2 + 1
+        answer += 1
+
+    return answer
