6a273f86288437d2b96e68fe4cbc0b1366b20bb9
Kim Hyung Un <84065420+khw3754@users.noreply.github.com>
2023-06-08 21:48:52 +0900

    [level 2] Title: 가장 큰 수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,9 +1,5 @@
 def solution(numbers):
-    numbers = list(map(str, numbers))
-    numbers = sorted(numbers, key=lambda x: x*3)
-    
-    result = ''.join(numbers[::-1])
-    if result[0] == '0':
-        return '0'
-    else:
-        return result
+    numbers = map(str, numbers)
+    numbers = sorted(numbers, key=lambda x: x*3, reverse=True)
+    answer = ''.join(numbers) if numbers[0] != '0' else '0'
+    return answer
