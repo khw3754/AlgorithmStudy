e634c49a3228da64e2d02d09638caf219c98507e
Kim Hyung Un <84065420+khw3754@users.noreply.github.com>
2023-10-05 00:50:02 +0900

    [Silver III] Title: N과 M (7), Time: 2360 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,15 @@
+N, M = map(int, input().split())
+nums = sorted(list(map(int, input().split())))
+
+def dfs(nums, list):
+    if len(list) == M:
+        print(*list)
+        return
+
+    for i in range(len(nums)):
+        copiedList = list[::]
+        copiedNums = nums[::]
+        copiedList.append(nums[i])
+        dfs(copiedNums, copiedList)
+
+dfs(nums, [])
